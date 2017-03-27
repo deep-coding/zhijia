f9c5fd16d88b698f3fb92ac0102b3ec9cf3a5446
lidengqi <lidengqi@sheyuan.com>
2017-03-27 13:50:44 +0800

    修改Tab子栏目资讯加载方式，当前页加载三次之后再跳转到列表页

--- src/main/java/com/jspxcms/core/web/fore/TabInfoListController.java
+++ src/main/java/com/jspxcms/core/web/fore/TabInfoListController.java
@@ -18,6 +18,7 @@ import java.util.Map;
 @Controller
 public class TabInfoListController {
     public static final String ZUIXIN_INFOLIST_TEMPLATE = "list_news_zuixin.html";
+    public static final String ZUIXIN_LOAD_INFOLIST_TEMPLATE = "list_news_zuixin_load.html";
     public static final String TAB_INFOLIST_TEMPLATE = "list_news_tab.html";
 
     @RequestMapping(value = {"/zuixin/list_news.jspx", Constants.SITE_PREFIX_PATH + "/zuixin/list_news.jspx"})
@@ -26,6 +27,14 @@ public class TabInfoListController {
         Map<String, Object> data = modelMap.asMap();
         ForeContext.setData(data, request);
         return site.getTemplate(ZUIXIN_INFOLIST_TEMPLATE);
+    }
+
+    @RequestMapping(value = {"/zuixin/load_more/list_news.jspx", Constants.SITE_PREFIX_PATH + "/zuixin/load_more/list_news.jspx"})
+    public String zuixinLoadInfoList(HttpServletRequest request, HttpServletResponse response, Model modelMap) {
+        Site site = Context.getCurrentSite();
+        Map<String, Object> data = modelMap.asMap();
+        ForeContext.setData(data, request);
+        return site.getTemplate(ZUIXIN_LOAD_INFOLIST_TEMPLATE);
     }
 
     @RequestMapping(value = {"/tab/list_news.jspx", Constants.SITE_PREFIX_PATH + "/tab/list_news.jspx"})
